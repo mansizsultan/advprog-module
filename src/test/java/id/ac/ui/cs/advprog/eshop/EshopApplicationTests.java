75eb301ce1578b64a7021660329075e65ff2789b
mansizsultan <sultanibnumansiz@gmail.com>
2025-02-21 19:54:55 +0700

    ADD additional test for better code coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,12 +2,19 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test verifies that the Spring application context loads successfully.
+        // No assertions are needed because failure to load the context will cause this test to fail.
     }
 
+    @Test
+    void testMainMethod() {
+        assertDoesNotThrow(() -> EshopApplication.main(new String[]{}));
+    }
 }
