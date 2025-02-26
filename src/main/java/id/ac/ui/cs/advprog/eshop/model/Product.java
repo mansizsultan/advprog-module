865878c66e3acc4a621db984639850122f17cca8
mansizsultan <sultanibnumansiz@gmail.com>
2025-02-26 23:55:54 +0700

    FIX src main

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import java.util.UUID;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.UUID;
 
-@Setter @Getter
+@Getter
+@Setter
 public class Product {
     private String productId;
+
+    @NotBlank(message = "Product name cannot be empty")
     private String productName;
+
+    @Min(value = 1, message = "Product quantity must be at least 1")
     private int productQuantity;
 
     public Product() {
