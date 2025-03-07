004b4aed08f25f8009b0661e4e2d7400f4aed278
mansizsultan <sultanibnumansiz@gmail.com>
2025-03-07 17:47:10 +0700

    [REFACTOR] Add OrderStatus enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -2,8 +2,8 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
+import enums.OrderStatus;
 import java.util.List;
 
 @Builder
@@ -13,11 +13,31 @@ public class Order {
     List<Product> products;
     Long orderTime;
     String author;
-
-    @Setter
     String status;
 
-    public Order(String id, List<Product> products, Long orderTime, String author) {}
+    public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
 
-    public Order(String id, List<Product> products, Long orderTime, String author, String status) {}
+        if (products.isEmpty()){
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
+    }
+
+    public Order (String id, List <Product> products, Long orderTime, String author, String status) {
+        this(id, products, orderTime, author);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (OrderStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
